d9ee5e588b3b57afd61900545d95d588968e2597
Aditya Pranav Bhuvanapalli <adityapranavbhuvanapalli@gmail.com>
2024-01-08 21:15:25 +0530

    Time: 3 ms (67.10%) | Memory: 12.1 MB (73.51%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -7,16 +7,23 @@ public:
         return res;
     }
 
-    void solve(int open, int close, int n, string comb) {
+    void solve(int open, int close, int n, string& comb) {
         if(close == n) {
             res.push_back(comb);
             return;
         }
 
-        if(open < n)
-            solve(open + 1, close, n, comb + '(');
+        if(open < n) {
+            comb += '(';
+            solve(open + 1, close, n, comb);
+            comb.pop_back();
 
-        if(close < open)
-            solve(open, close + 1, n, comb + ')');
+        }
+
+        if(close < open) {
+            comb += ')';
+            solve(open, close + 1, n, comb);
+            comb.pop_back();
+        }
     }
 };
