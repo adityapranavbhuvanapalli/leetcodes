e3ea5a0799e0b97fa3e3e4b4b0741e82a54fa150
Aditya Pranav Bhuvanapalli <adityapranavbhuvanapalli@gmail.com>
2024-01-12 09:18:00 +0530

    Time: 0 ms (100.00%) | Memory: 7.1 MB (46.92%) - LeetSync

--- 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.cpp
+++ 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.cpp
@@ -3,14 +3,14 @@ public:
     bool halvesAreAlike(string s) {
         char ch;
         int count = 0, l = 0, r = s.size() - 1;
-        
-        while(l < r) {
+
+        while (l < r) {
             ch = tolower(s[l]);
-            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
                 count++;
 
             ch = tolower(s[r]);
-            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
                 count--;
 
             l++, r--;
