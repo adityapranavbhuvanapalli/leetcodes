7bce5fe26de1f48b3b91f3ac0cc99ccd75580c3d
Aditya Pranav Bhuvanapalli <adityapranavbhuvanapalli@gmail.com>
2024-01-13 13:39:09 +0530

    Time: 216 ms (46.55%) | Memory: 121.4 MB (39.66%) - LeetSync

--- 2661-smallest-missing-non-negative-integer-after-operations/smallest-missing-non-negative-integer-after-operations.cpp
+++ 2661-smallest-missing-non-negative-integer-after-operations/smallest-missing-non-negative-integer-after-operations.cpp
@@ -1,36 +1,22 @@
 class Solution {
 public:
     int findSmallestInteger(vector<int>& nums, int value) {
-        int n = nums.size();
+        int n = nums.size(), key;
         unordered_map<int, int> remainders;
 
-        for(auto& num: nums) {
+        for (auto& num : nums) {
             num = num % value;
-            if(num < 0) {
-                remainders[value + num]++;
-            } else {
-                remainders[num]++;
-            }
+            key = num < 0 ? value + num : num;
+            num = key + remainders[key] * value;
+            remainders[key]++;
         }
-
-        int i=0;
-        for(auto& [k, v]: remainders) {
-            // cout<<k<<": "<<v<<endl;
-            while(v--)
-                nums[i++] = k + v * value;
-        }
-
-        // for(const auto& num: nums)
-        //     cout<<num<<" ";
 
         // First missing non - negative
-        i = 0;
-        while (i < n) {
+        for (int i = 0; i < n; i++) {
             while (i < n && nums[i] < n && nums[i] != i &&
                    nums[nums[i]] != nums[i]) {
                 swap(nums[i], nums[nums[i]]);
             }
-            i++;
         }
 
         for (int i = 0; i < n; i++) {
