7741531831c3e8fbd01378203b529569cede3742
Aditya Pranav Bhuvanapalli <adityapranavbhuvanapalli@gmail.com>
2024-01-13 15:10:27 +0530

    Time: 103 ms (84.05%) | Memory: 29.1 MB (90.89%) - LeetSync

--- 2293-minimum-number-of-steps-to-make-two-strings-anagram-ii/minimum-number-of-steps-to-make-two-strings-anagram-ii.cpp
+++ 2293-minimum-number-of-steps-to-make-two-strings-anagram-ii/minimum-number-of-steps-to-make-two-strings-anagram-ii.cpp
@@ -2,23 +2,16 @@ class Solution {
 public:
     int minSteps(string s, string t) {
         int count = 0;
-        vector<int> M(26, 0);
+        int M[26] = {0};
 
-        for(auto& c: s) {
-            count++;
+        for(const auto& c: s)
             M[c - 'a']++;
-        }
 
-        for(auto& c: t) {
-            if(M[c - 'a'] > 0)
-                count--;
-            else 
-                count++;
+        for(const auto& c: t)
             M[c - 'a']--;
-        }
 
-        // for(auto& [k, v]: M)
-        //     count += abs(v);
+        for(int i=0; i<26; i++)
+            count += abs(M[i]);
 
         return count;
     }
