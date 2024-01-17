2355385adaf26b715934a4fba220cf1f0771c1ae
Aditya Pranav Bhuvanapalli <adityapranavbhuvanapalli@gmail.com>
2024-01-17 14:07:53 +0530

    Time: 534 ms (16.12%) | Memory: 191 MB (5.15%) - LeetSync

--- 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.cpp
+++ 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.cpp
@@ -3,20 +3,25 @@ public:
     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
         int n1 = nums1.size(), n2 = nums2.size();
         vector<vector<int>> res;
-        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+
+        auto compare = [&](auto& l, auto& r) {
+            return (nums1[l[0]] + nums2[l[1]]) > (nums1[r[0]] + nums2[r[1]]);
+        };
+
+        priority_queue<vector<int>, vector<vector<int>>, decltype(compare)> pq(compare);
 
         for(int i=0; i<n1; i++) {
-            pq.push({nums1[i] + nums2[0], i, 0});
+            pq.push({i, 0});
         }
 
         while(k-- && pq.size()) {
             auto v = pq.top();
             pq.pop();
 
-            res.push_back({nums1[v[1]], nums2[v[2]]});
+            res.push_back({nums1[v[0]], nums2[v[1]]});
 
-            if(v[2] < n2-1)
-                pq.push({nums1[v[1]] + nums2[v[2]+1], v[1], v[2]+1});
+            if(v[1] < n2-1)
+                pq.push({v[0], v[1]+1});
         }
 
         return res;
