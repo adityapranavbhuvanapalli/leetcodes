7846a6a430d0280bbc1d24814801cfb9efc7069c
Aditya Pranav Bhuvanapalli <adityapranavbhuvanapalli@gmail.com>
2024-01-18 10:51:45 +0530

    Time: 3 ms (47.11%) | Memory: 7 MB (56.65%) - LeetSync

--- 388-longest-absolute-file-path/longest-absolute-file-path.cpp
+++ 388-longest-absolute-file-path/longest-absolute-file-path.cpp
@@ -9,8 +9,6 @@ public:
 
         input += "\n";
 
-        cout<<input<<endl<<endl;
-
         for(const auto& ch: input) {
             if(ch == '\t') {
                 tabs++;
@@ -18,7 +16,6 @@ public:
             }
 
             if(ch == '\n') {
-                cout<<str<<"("<<tabs<<"): "<<endl;
                 while(tabs < s.size()) {
                     len -= s.top().size();
                     s.pop();
@@ -26,16 +23,9 @@ public:
 
                 s.push(str);
                 len += str.size();
-                stack<string> tmp(s);
-                while(tmp.size()) {
-                    cout<<tmp.top()<<"<-";
-                    tmp.pop();
-                }
-                cout<<endl;
-
                 str = "";
                 tabs = 0;
-                // MARK: If str is a file name, then update maxLen
+                
                 if(isFile) {
                     maxLen = max(maxLen, len + (int)s.size() - 1);
                     isFile = false;
@@ -44,11 +34,9 @@ public:
                 continue;
             }
 
-            // cout<<ch<<": "<<endl;
             str += ch;
             if(ch == '.')
                 isFile = true;
-
         }
 
         return maxLen;
