7d10d8dd1900b760df239de37030b60851dfc8bc
Aditya Pranav Bhuvanapalli <adityapranavbhuvanapalli@gmail.com>
2024-01-19 11:25:44 +0530

    Time: 137 ms (24.30%) | Memory: 94.7 MB (62.57%) - LeetSync

--- 951-partition-array-into-disjoint-intervals/partition-array-into-disjoint-intervals.cpp
+++ 951-partition-array-into-disjoint-intervals/partition-array-into-disjoint-intervals.cpp
@@ -5,22 +5,14 @@ public:
         stack<int> s;
 
         for(int i=n-1; i>=0; i--) {
-            if(!s.size() || nums[s.top()] > nums[i]) {
+            if(!s.size() || nums[s.top()] > nums[i])
                 s.push(i);
-            }
         }
 
         for(int i=0; i<n; i++) {
             maxSoFar = max(maxSoFar, nums[i]);
             if(s.size() && s.top() == i)
                 s.pop();
-
-            // stack<int> tmp(s);
-            // while(tmp.size()) {
-            //     cout<<tmp.top()<<"<";
-            //     tmp.pop();
-            // }
-            // cout<<endl;
 
             if(maxSoFar <= nums[s.top()]) 
                 return i+1;
