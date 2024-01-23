0baebd23ec5cba8919aa961c22cd5a20f7d6189f
Aditya Pranav Bhuvanapalli <adityapranavbhuvanapalli@gmail.com>
2024-01-23 09:10:11 +0530

    Time: 6 ms (88.42%) | Memory: 9.6 MB (82.42%) - LeetSync

---
+++ 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int maxLength(vector<string>& arr) {
+        int n = arr.size();
+        // vector<vector<int>> dp(n, vector<int>(INT_MAX, -1));
+        return solve(0, 0, n, arr);
+    }
+
+    int solve(int i, int mask, int n, vector<string>& arr) {
+        if(i >= n)
+            return 0;
+
+        // if(dp[i][mask] != -1)
+        //     return dp[i][mask];
+
+        int notpick = solve(i+1, mask, n, arr);
+
+        for(const auto& ch: arr[i]) {
+            if(mask & (1 << (ch - 'a')))
+                return notpick;
+
+            mask = mask | (1 << (ch - 'a')); 
+        }
+        
+        int pick = arr[i].size() + solve(i, mask, n, arr);
+
+        return max(pick, notpick);
+    }
+};
+
+
+/*
+00000000000000000000000
+mask | (1<<ch)
+
+2^27
+
+(2^4)^7
+
+8^9
+*/
