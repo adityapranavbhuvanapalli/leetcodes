6ed7734cd66fc5b7feccf2f2547f3aa2ba78c0c3
Aditya Pranav Bhuvanapalli <adityapranavbhuvanapalli@gmail.com>
2024-01-24 16:47:12 +0530

    Time: 5 ms (89.79%) | Memory: 10.1 MB (7.14%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        int l = 0, r = 0, n = s.size(), count = 0, minLen = INT_MAX, start = -1;
+        vector<int> freqS(256, 0), freqT(256, 0);
+
+        for(const auto& ch: t)
+            freqT[ch]++;
+
+        for(const auto& x: freqT)
+            count += (x > 0);
+
+        while(r < n) {
+            // cout<<l<<" "<<r;
+            
+            freqS[s[r]]++;
+            if(freqS[s[r]] == freqT[s[r]])
+                count--;
+            r++;
+
+            while(l < r && count == 0) {
+                if(minLen > r - l) {
+                    start = l;
+                    minLen = r - l;
+                }
+
+                if(freqS[s[l]] == freqT[s[l]])
+                    count++;
+                freqS[s[l]]--;
+                l++;
+            }
+            // cout<<" "<<minLen<<endl;
+        }
+
+        return minLen == INT_MAX ? "" : s.substr(start, minLen);
+    }
+};
+
+/*
+ABC
+
+ADOBECODEBANC
+-l
+----------r
+*/
