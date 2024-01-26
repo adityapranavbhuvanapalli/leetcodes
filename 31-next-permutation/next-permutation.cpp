4522079fec36e0c6e57c7aede79b1ea7593222eb
Aditya Pranav Bhuvanapalli <adityapranavbhuvanapalli@gmail.com>
2024-01-27 00:56:44 +0530

    Time: 0 ms (100.00%) | Memory: 14.5 MB (8.92%) - LeetSync

---
+++ 31-next-permutation/next-permutation.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int index, n = nums.size();
+
+        for(int i=n-2; i>=0; i--) {
+            if(nums[i] < nums[i+1]) {
+                index = i;
+                break;
+            }
+        }
+
+        if(index == -1) {
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+
+         for(int i=n-1; i>=0; i--) {
+            if(nums[i] > nums[index]) {
+                swap(nums[i], nums[index]);
+                break;
+            }
+        }
+         
+        reverse(nums.begin() + index + 1, nums.end());
+    }
+};
+
+/*
+1   2   3   4
+1   2   4   3
+1   3   2   4
+1   3   4   2
+1 
+.
+.
+.
+4   3   2   1
+*/
