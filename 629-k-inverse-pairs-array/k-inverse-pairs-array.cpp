bdc1b63a9327d5c6003768e3f0a6763da9862a2a
Aditya Pranav Bhuvanapalli <adityapranavbhuvanapalli@gmail.com>
2024-01-27 13:19:10 +0530

    Time: 1320 ms (15.93%) | Memory: 17.5 MB (10.99%) - LeetSync

--- 629-k-inverse-pairs-array/k-inverse-pairs-array.cpp
+++ 629-k-inverse-pairs-array/k-inverse-pairs-array.cpp
@@ -6,16 +6,13 @@ public:
 
         dp[1][0] = 1;
         for(int i=1; i<=n; i++) {
-            // cout<<i<<": ";
             for(int j=0; j<=k; j++) {
                 for(int x=0; x<i; x++) {
                     if(x > j)
                         break;
                     dp[i][j] = (dp[i][j] + dp[i-1][j-x]) % MOD;
                 }
-                // cout<<"\t"<<dp[i][j];
             }
-            // cout<<endl;
         } 
 
         return dp[n][k];
