6b620d9ec514d497a3c00668567e10db55be68fd
Aditya Pranav Bhuvanapalli <adityapranavbhuvanapalli@gmail.com>
2024-01-27 09:00:30 +0530

    Time: 4 ms (81.94%) | Memory: 10.8 MB (22.82%) - LeetSync

--- 576-out-of-boundary-paths/out-of-boundary-paths.cpp
+++ 576-out-of-boundary-paths/out-of-boundary-paths.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    int MOD = 1e9 + 7;
-    int findPaths(int m, int n, int maxMoves, int i, int j) {
-        vector<vector<vector<int>>> dp(
-            maxMoves + 1, vector<vector<int>>(m + 1, vector<int>(n + 1, -1)));
-        return solve(m, n, maxMoves, i, j, dp);
+    const int MOD = 1e9 + 7;
+    vector<vector<vector<int>>> dp;
+    int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+        dp = vector<vector<vector<int>>>(
+            maxMove + 1, vector<vector<int>>(m, vector<int>(n, -1)));
+        return solve(startRow, startColumn, m, n, maxMove);
     }
 
-    int solve(int m, int n, int M, int i, int j,
-              vector<vector<vector<int>>>& dp) {
+    int solve(int i, int j, int m, int n, int M) {
         if (i < 0 || j < 0 || i >= m || j >= n)
             return 1;
 
@@ -19,10 +19,11 @@ public:
             return dp[M][i][j];
 
         int res = 0;
-        res = (res + solve(m, n, M - 1, i - 1, j, dp)) % MOD;
-        res = (res + solve(m, n, M - 1, i + 1, j, dp)) % MOD;
-        res = (res + solve(m, n, M - 1, i, j - 1, dp)) % MOD;
-        res = (res + solve(m, n, M - 1, i, j + 1, dp)) % MOD;
+
+        res = (res + solve(i - 1, j, m, n, M - 1)) % MOD;
+        res = (res + solve(i + 1, j, m, n, M - 1)) % MOD;
+        res = (res + solve(i, j - 1, m, n, M - 1)) % MOD;
+        res = (res + solve(i, j + 1, m, n, M - 1)) % MOD;
 
         return dp[M][i][j] = res;
     }
