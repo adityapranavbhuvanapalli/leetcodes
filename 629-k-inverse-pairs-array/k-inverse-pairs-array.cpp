8b04217cb185c22226636c4eaba8bd3587101c58
Aditya Pranav Bhuvanapalli <adityapranavbhuvanapalli@gmail.com>
2024-01-28 21:23:25 +0530

    Time: 1764 ms (10.27%) | Memory: 17.4 MB (14.05%) - LeetSync

--- 629-k-inverse-pairs-array/k-inverse-pairs-array.cpp
+++ 629-k-inverse-pairs-array/k-inverse-pairs-array.cpp
@@ -2,33 +2,26 @@ class Solution {
 public:
     const int MOD = 1e9 + 7;
     int kInversePairs(int n, int k) {
-        bool flag;
         vector<vector<int>> dp(n + 1, vector<int>(k + 1, 0));
 
         dp[1][0] = 1;
-        for(int i=1; i<=n; i++) {
-            flag = false;
+        for(int i=2; i<=n; i++) {
             for(int j=0; j<=k; j++) {
                 for(int x=0; x<i; x++) {
-                    if(x > j)
+                    if(j-x < 0)
                         break;
-                    
                     dp[i][j] = (dp[i][j] + dp[i-1][j-x]) % MOD;
                 }
-
-                if(dp[i][j] == 0)
-                    break;
             }
-        } 
+        }
 
         return dp[n][k];
     }
 };
 
 /*
-1 - 0
-2 - 01
-3 - 01 12 23 
-4 - 011223 122334 233445 344556
-    0      1      2      3  
+01 - 0(1), 1(1)
+011223 - 0(1), 1(2), 2(2), 3(1)
+011223 122334 233445 344556 - 0(1), 1()
+0      1      2      3
 */
