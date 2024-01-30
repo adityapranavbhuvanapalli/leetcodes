d42e1fc6941be34172a7ed01a4f7886e49270320
Aditya Pranav Bhuvanapalli <adityapranavbhuvanapalli@gmail.com>
2024-01-30 08:13:43 +0530

    Time: 6 ms (86.57%) | Memory: 15.4 MB (10.51%) - LeetSync

---
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int> s;
+        int op1, op2;
+        for(auto& token: tokens) {
+            if(token == "+" || token == "-" || token == "*" || token == "/") {
+                op1 = s.top(), s.pop();
+                op2 = s.top(), s.pop();
+                if(token == "+")
+                    s.push(op1 + op2);
+                else if(token == "-")
+                    s.push(op2 - op1);
+                if(token == "*")
+                    s.push(op1 * op2);
+                else if(token == "/")
+                    s.push(op2 / op1);
+            } else {
+                s.push(stoi(token));
+            }
+        }
+
+        return s.top();
+    }
+};
