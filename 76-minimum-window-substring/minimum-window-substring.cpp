93b031c023ed09931c9a3e458631c1fe582624a4
Aditya Pranav Bhuvanapalli <adityapranavbhuvanapalli@gmail.com>
2024-02-05 09:02:16 +0530

    Time: 5 ms (89.58%) | Memory: 9.9 MB (17.81%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -4,13 +4,12 @@ public:
         int l = 0, r = 0, n = s.size(), count = 0, minLen = INT_MAX, start = -1;
         vector<int> freqS(256, 0), freqT(256, 0);
 
-        for(const auto& ch: t)
-            freqT[ch]++;
+        for(const auto& ch: t) {
+            if(freqT[ch]++ == 0)
+                count++;
+        }
 
-        for(const auto& x: freqT)
-            count += (x > 0);
-
-        while(r < n) {
+        while(r < n) {  
             freqS[s[r]]++;
             if(freqS[s[r]] == freqT[s[r]])
                 count--;
