6b714ef11254e0e7e8edacc1cf5cb0071f59740d
Aditya Pranav Bhuvanapalli <adityapranavbhuvanapalli@gmail.com>
2024-02-08 09:43:20 +0530

    Time: 2531 ms (5.13%) | Memory: 339.7 MB (13.85%) - LeetSync

---
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    const int MOD = 1e9 + 7;
+    map<pair<int, int>, int> dp;
+    int numberOfWays(int n, int x) {
+        return solve(1, n, x);
+    }
+
+    int solve(int i, int n, int x) {
+        if(n == 0)
+            return 1;
+
+        if(n < 0 || pow(i, x) > n)
+            return 0;
+
+        if(dp.count({i, n}) == 1)
+            return dp[{i, n}];
+
+        int pick = solve(i + 1, n - pow(i, x), x);
+        int notpick = solve(i+1, n, x);
+
+        return dp[{i, n}] = (pick + notpick) % MOD;
+    }
+};
+
+/*
+Unique bases
+
+1 - 1^2 = 1 ways
+2 - 0 ways
+3 - 0 ways
+4 - 2^2 = 1 way
+5 - 2^2 + 1^2 = 1 way
+*/
