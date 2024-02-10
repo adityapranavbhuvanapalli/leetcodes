da2d57e68106a350d360d67e65746be72e93982f
Aditya Pranav Bhuvanapalli <adityapranavbhuvanapalli@gmail.com>
2024-02-10 23:59:23 +0530

    Time: 651 ms (97.35%) | Memory: 275.2 MB (85.78%) - LeetSync

--- 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.cpp
+++ 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.cpp
@@ -6,7 +6,8 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
@@ -15,25 +16,25 @@ public:
         unordered_map<int, TreeNode*> address;
         unordered_map<int, int> incoming;
 
-        for(const auto& desc: descriptions) {
+        for (const auto& desc : descriptions) {
             incoming[desc[1]]++;
-            if(incoming.count(desc[0]) == 0)
+            if (incoming.count(desc[0]) == 0)
                 incoming[desc[0]] = 0;
 
-            if(address.count(desc[0]) == 0)
+            if (address.count(desc[0]) == 0)
                 address[desc[0]] = new TreeNode(desc[0]);
 
-            if(address.count(desc[1]) == 0)
+            if (address.count(desc[1]) == 0)
                 address[desc[1]] = new TreeNode(desc[1]);
 
-            if(desc[2])
+            if (desc[2])
                 address[desc[0]]->left = address[desc[1]];
             else
                 address[desc[0]]->right = address[desc[1]];
         }
 
-        for(const auto& [k, v]: incoming)
-            if(v == 0)
+        for (const auto& [k, v] : incoming)
+            if (v == 0)
                 return address[k];
 
         return nullptr;
