5c304a4bdd09539cdee336547adae24559ce020c
Aditya Pranav Bhuvanapalli <adityapranavbhuvanapalli@gmail.com>
2024-02-12 16:34:30 +0530

    Time: 12 ms (54.63%) | Memory: 21 MB (11.20%) - LeetSync

---
+++ 950-x-of-a-kind-in-a-deck-of-cards/x-of-a-kind-in-a-deck-of-cards.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool hasGroupsSizeX(vector<int>& deck) {
+        int x = -1;
+        unordered_map<int, int> count;
+
+        for(const auto& d: deck)
+            count[d]++;
+
+
+        for(const auto& [k, v]: count) {
+            if(x == -1) {
+                x = v;
+                continue;
+            }
+
+            x = __gcd(x, v);
+        }
+
+        return x > 1;
+    }
+};
