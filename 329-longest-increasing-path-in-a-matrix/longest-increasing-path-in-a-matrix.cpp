aaa7631f368dd090a842ca39f2300d5766a15974
Aditya Pranav Bhuvanapalli <adityapranavbhuvanapalli@gmail.com>
2024-02-13 21:15:50 +0530

    Time: 31 ms (76.38%) | Memory: 18.6 MB (51.76%) - LeetSync

--- 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
@@ -1,35 +1,41 @@
 class Solution {
 public:
+    int m, n;
+    vector<vector<int>> dp;
     int longestIncreasingPath(vector<vector<int>>& matrix) {
-        int m = matrix.size(), n = matrix[0].size(), res = 0;
-        vector<vector<int>> dp(m, vector<int>(n, -1));
+        m = matrix.size(), n = matrix[0].size();
+        int res = 0;
 
-        for(int i=0; i<m; i++)
-            for(int j=0; j<n; j++)
-                res = max(res, dfs(i, j, m, n, matrix, dp));
+        dp = vector<vector<int>>(m, vector<int>(n, -1));
 
-        return res;
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                res = max(res, dfs(i, j, matrix));
+            }
+        }   
+
+        return 1 + res;
     }
 
-    int dfs(int x, int y, int m, int n, vector<vector<int>>& matrix, vector<vector<int>>& dp) {
+    int dfs(int x, int y, vector<vector<int>>& matrix) {
         if(dp[x][y] != -1)
             return dp[x][y];
 
-        int res = 0, tmp = matrix[x][y];
+        int tmp = matrix[x][y], res = 0;
 
         matrix[x][y] = -1;
 
-        if(x > 0 && matrix[x-1][y] > tmp)
-            res = max(res, dfs(x-1, y, m, n, matrix, dp));
-        if(x < m-1 && matrix[x+1][y] > tmp)
-            res = max(res, dfs(x+1, y, m, n, matrix, dp));
-        if(y > 0 && matrix[x][y-1] > tmp)
-            res = max(res, dfs(x, y-1, m, n, matrix, dp));
-        if(y < n-1 && matrix[x][y+1] > tmp)
-            res = max(res, dfs(x, y+1, m, n, matrix, dp));
+        if(x > 0 && matrix[x - 1][y] > tmp)
+            res = max(res, 1 + dfs(x - 1, y, matrix));
+        if(x < m - 1 && matrix[x + 1][y] > tmp)
+            res = max(res, 1 + dfs(x + 1, y, matrix));
+        if(y > 0 && matrix[x][y - 1] > tmp)
+            res = max(res, 1 + dfs(x, y - 1, matrix));
+        if(y < n - 1 && matrix[x][y + 1] > tmp)
+            res = max(res, 1 + dfs(x, y + 1, matrix));
 
         matrix[x][y] = tmp;
 
-        return dp[x][y] = 1 + res;
+        return dp[x][y] = res;
     }
 };
