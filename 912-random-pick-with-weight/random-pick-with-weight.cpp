6f30bd62bd38787bab8df7143205e8b71c3c50ee
Aditya Pranav Bhuvanapalli <adityapranavbhuvanapalli@gmail.com>
2024-02-13 23:08:29 +0530

    Time: 56 ms (86.41%) | Memory: 44 MB (12.18%) - LeetSync

--- 912-random-pick-with-weight/random-pick-with-weight.cpp
+++ 912-random-pick-with-weight/random-pick-with-weight.cpp
@@ -1,48 +1,42 @@
 class Solution {
 public:
+    int n, limit = 0;
     vector<int> prefix;
-    int n, limit;
     Solution(vector<int>& w) {
-        int sum = 0;
-        prefix.resize(w.size());
         n = w.size();
-        for(int i=0; i<n; i++) {
-            sum += w[i];
-            prefix[i] = sum;
+        for(const auto& e: w) {
+            limit += e;
+            prefix.push_back(limit);
         }
-
-        limit = sum;
     }
-
+    
     int pickIndex() {
-        int random = rand() % limit;
-        int l = 0, r = n-1, mid;
+        int randIdx = rand() % limit;
+
+        int l = 0, r = n - 1, mid;
+
         while(l < r) {
             mid = l + (r - l) / 2;
 
-            if(random < prefix[mid]) {
+            if(prefix[mid] > randIdx) {
                 r = mid;
             } else {
                 l = mid + 1;
             }
         }
-        
+
         return l;
     }
 };
 
 /*
+0   1   1   1
 
-1   2   3   4   5
-
-1   2   1   2   3
-1   3   4   6   9     / 9
-0   1   2   3   4
-
-
-5, 6
-3, 3
+1   3
+1 234
+0   1
 */
+
 
 /**
  * Your Solution object will be instantiated and called as such:
