0e957666f83410acabdae18c3283d6d0ac172fa9
Aditya Pranav Bhuvanapalli <adityapranavbhuvanapalli@gmail.com>
2024-02-14 20:30:07 +0530

    Time: 163 ms (71.04%) | Memory: 55 MB (49.40%) - LeetSync

--- 808-number-of-matching-subsequences/number-of-matching-subsequences.cpp
+++ 808-number-of-matching-subsequences/number-of-matching-subsequences.cpp
@@ -1,61 +1,57 @@
 class Solution {
 public:
     int numMatchingSubseq(string s, vector<string>& words) {
-        int n = s.size(), idx, count = words.size();
+        int n = s.size(), idx, count = words.size(), i;
         unordered_map<char, vector<int>> positions;
 
-        for(int i=0; i<n; i++) {
+        for(int i=0; i<n; i++)
             positions[s[i]].push_back(i);
-        }
 
-        for(const auto& word: words) {
+        for(auto& word: words) {
             idx = -1;
+            i = 0;
             for(const auto& ch: word) {
                 idx = binarySearch(positions[ch], idx + 1);
                 if(idx == -1) {
+                    // Not a subsequence
                     count--;
                     break;
                 }
-            } 
+            }
         }
 
         return count;
     }
 
-    int binarySearch(vector<int>& positions, int x) {
-        int n = positions.size(), l = 0, r = n - 1, mid;
-
-        if(n == 0 || positions.back() < x)
+    int binarySearch(vector<int>& nums, int key) {
+        // Finds a value in nums greater than or equal to the key
+        int n = nums.size();
+        if(n == 0 || key > nums.back())
             return -1;
 
+        // Need to find the first occuring number which is >= key
+        int l = 0, r = n - 1, mid;
         while(l < r) {
-            mid = l + (r-l)/2;
+            mid = l + (r - l) / 2;
 
-            if(positions[mid] >= x) {
+            if(nums[mid] >= key) {
                 r = mid;
             } else {
                 l = mid + 1;
             }
         }
 
-        return positions[l];
+        return nums[l];
     }
 };
 
 /*
-abcde
-a
-bb
-acd
-ace
-
-abdcdce
-0 23
-acd
-032
-
-Using binary search
-
-
-0   3   4   6   11   20
+d - 0
+s - 1
+a - 2   7
+h - 3
+j - 4   6
+p - 5
+u - 8 
+f - 9
 */
