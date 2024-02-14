3cd6874eee1f55d7434909d62bf4ece93ca00e7b
Aditya Pranav Bhuvanapalli <adityapranavbhuvanapalli@gmail.com>
2024-02-14 14:18:56 +0530

    Time: 50 ms (16.06%) | Memory: 17.1 MB (11.17%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.cpp
+++ 794-swim-in-rising-water/swim-in-rising-water.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     int swimInWater(vector<vector<int>>& grid) {
-        int n = grid.size(), minTime = INT_MAX;
+        int n = grid.size();
         priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
         vector<pair<int, int>> dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
         vector<vector<int>> visited(n, vector<int>(n, 0));
-        vector<vector<int>> dist(n, vector<int>(n, INT_MAX));
 
         pq.push({grid[0][0], 0, 0});
         while(pq.size()) {
@@ -14,27 +13,21 @@ public:
             int y = pq.top()[2];
             pq.pop();
 
-            // cout<<x<<" "<<y<<" "<<time<<endl;
-            dist[x][y] = time;
-
             if(x == n-1 && y == n-1)
                 return time;
 
             for(int k=0; k<4; k++) {
-                if(visited[x][y] & (1 << k))
+                if(visited[x][y] & (1<<k))
                     continue;
-                    
+
                 auto& [dx, dy] = dirs[k];
                 int X = x + dx, Y = y + dy;
+
                 if(X < 0 || X >= n || Y < 0 || Y >= n)
                     continue;
-                
-                if(dist[X][Y] > time + max(0, grid[X][Y] - grid[x][y])) {
-                    visited[x][y] = visited[x][y] | (1 << k);
-                    pq.push({max(time, grid[X][Y]), X, Y}); 
-                }
 
-                // cout<<"\t"<<X<<" "<<Y<<" "<<time + grid[X][Y] - grid[x][y]<<endl;
+                visited[x][y] = visited[x][y] | (1 << k);
+                pq.push({max(time, grid[X][Y]), X, Y});
             }
         }
 
