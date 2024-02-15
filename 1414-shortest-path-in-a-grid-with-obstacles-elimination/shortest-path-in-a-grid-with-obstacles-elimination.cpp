58f0a74dac9fe2f0c648b19605829396c64a2890
Aditya Pranav Bhuvanapalli <adityapranavbhuvanapalli@gmail.com>
2024-02-15 13:53:04 +0530

    Time: 56 ms (68.71%) | Memory: 20.3 MB (67.30%) - LeetSync

--- 1414-shortest-path-in-a-grid-with-obstacles-elimination/shortest-path-in-a-grid-with-obstacles-elimination.cpp
+++ 1414-shortest-path-in-a-grid-with-obstacles-elimination/shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -1,42 +1,36 @@
 class Solution {
 public:
-    int shortestPath(vector<vector<int>>& grid, int K) {
+    int shortestPath(vector<vector<int>>& grid, int k) {
         int m = grid.size(), n = grid[0].size(), sz, steps = 0;
         queue<tuple<int, int, int>> q;
-        vector<vector<vector<bool>>> visited(m, vector<vector<bool>>(n, vector<bool>(K + 1, false)));
+        vector<vector<vector<bool>>> visited(m + 1, vector<vector<bool>>(n + 1, vector<bool>(k + 1, false)));
 
-        q.push({0, 0, K});
-        visited[0][0][K] = true;
+        q.push({0, 0, k});
+        visited[0][0][k] = true;
         while(q.size()) {
             sz = q.size();
             while(sz--) {
                 auto& [x, y, k] = q.front();
 
-                cout<<x<<" "<<y<<" "<<k<<endl;
-
                 if(x == m - 1 && y == n - 1)
                     return steps;
 
                 if(x > 0 && k >= grid[x - 1][y] && !visited[x - 1][y][k - grid[x - 1][y]]) {
-                    // cout<<"\t1. "<<x - 1<<" "<<y<<" "<<k - grid[x - 1][y]<<endl;
                     q.push({x - 1, y, k - grid[x - 1][y]});
                     visited[x - 1][y][k - grid[x - 1][y]] = true;
                 }
 
                 if(x < m - 1 && k >= grid[x + 1][y] && !visited[x + 1][y][k - grid[x + 1][y]]) {
-                    // cout<<"\t2. "<<x + 1<<" "<<y<<" "<<k - grid[x + 1][y]<<endl;
                     q.push({x + 1, y, k - grid[x + 1][y]});
                     visited[x + 1][y][k - grid[x + 1][y]] = true;
                 }
 
                 if(y > 0 && k >= grid[x][y - 1] && !visited[x][y - 1][k - grid[x][y - 1]]) {
-                    // cout<<"\t3. "<<x<<" "<<y - 1<<" "<<k - grid[x][y - 1]<<endl;
                     q.push({x, y - 1, k - grid[x][y - 1]});
                     visited[x][y - 1][k - grid[x][y - 1]] = true;
                 }
 
                 if(y < n - 1 && k >= grid[x][y + 1] && !visited[x][y + 1][k - grid[x][y + 1]]) {
-                    // cout<<"\t4. "<<x<<" "<<y + 1<<" "<<k - grid[x][y + 1]<<endl;
                     q.push({x, y + 1, k - grid[x][y + 1]});
                     visited[x][y + 1][k - grid[x][y + 1]] = true;
                 }
@@ -44,7 +38,6 @@ public:
                 q.pop();
             }
 
-            cout<<"-------"<<endl;
             steps++;
         }
 
