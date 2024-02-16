2ae202791742b7fbd627f625607f21c69b5d9a1d
Aditya Pranav Bhuvanapalli <adityapranavbhuvanapalli@gmail.com>
2024-02-16 09:30:16 +0530

    Time: 396 ms (69.66%) | Memory: 128.6 MB (41.95%) - LeetSync

---
+++ 2123-the-number-of-weak-characters-in-the-game/the-number-of-weak-characters-in-the-game.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int numberOfWeakCharacters(vector<vector<int>>& properties) {
+        int n = properties.size(), maxVal = 0, count = 0;
+
+        sort(properties.begin(), properties.end(), [&](auto& l, auto& r) {
+            return l[0] == r[0] ? l[1] > r[1] : l[0] < r[0];
+        });
+        
+        for(int i=n-1; i>=0; i--) {
+            if(properties[i][1] < maxVal)
+                count++;
+
+            maxVal = max(maxVal, properties[i][1]);
+        }
+
+        return count;
+    }
+};
+
+/*
+0   3,6
+1   5,4
+2   5,5
+3   6,3
+
+Sort based on Attack so we have to consider only Defence 
+Iterate from end and keep track of max value so far
+*/
