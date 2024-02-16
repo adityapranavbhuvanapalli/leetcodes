c39d39eaf396959f5c2a1ae88f2b99a270540c7f
Aditya Pranav Bhuvanapalli <adityapranavbhuvanapalli@gmail.com>
2024-02-16 16:14:30 +0530

    Time: 287 ms (74.75%) | Memory: 163.9 MB (18.47%) - LeetSync

--- 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.cpp
+++ 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.cpp
@@ -2,31 +2,34 @@ class Solution {
 public:
     vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) {
         int n = recipes.size();
+        vector<string> res;
+        unordered_set<string> recipeSet(recipes.begin(), recipes.end());
         unordered_map<string, vector<string>> graph;
         unordered_map<string, int> incoming;
-        unordered_set<string> recipeList(recipes.begin(), recipes.end());
-        vector<string> res;
         queue<string> q;
 
         for(int i=0; i<n; i++) {
-            for(const auto& ingredient: ingredients[i]) {
-                graph[ingredient].push_back(recipes[i]);
+            for(int j=0; j<ingredients[i].size(); j++) {
                 incoming[recipes[i]]++;
+                graph[ingredients[i][j]].push_back(recipes[i]);
             }
         }
 
-        for(const auto& supply: supplies)
+        // Topological Sort
+        for(auto& supply: supplies)
             q.push(supply);
 
         while(q.size()) {
             auto& cur = q.front();
-            
-            if(recipeList.count(cur))
+
+            if(recipeSet.count(cur))
                 res.push_back(cur);
 
-            for(const auto& adj: graph[cur]) {
-                if(--incoming[adj] == 0)
+            for(auto& adj: graph[cur]) {
+                incoming[adj]--;
+                if(incoming[adj] == 0) {
                     q.push(adj);
+                }
             }
 
             q.pop();
