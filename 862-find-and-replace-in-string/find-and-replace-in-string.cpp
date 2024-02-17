7f7e13afc7035b165fd008b863cd02b982fbd28b
Aditya Pranav Bhuvanapalli <adityapranavbhuvanapalli@gmail.com>
2024-02-17 10:48:23 +0530

    Time: 4 ms (66.02%) | Memory: 14.5 MB (20.19%) - LeetSync

--- 862-find-and-replace-in-string/find-and-replace-in-string.cpp
+++ 862-find-and-replace-in-string/find-and-replace-in-string.cpp
@@ -3,12 +3,10 @@ public:
     string findReplaceString(string s, vector<int>& indices, vector<string>& sources, vector<string>& targets) {
         int i = 0, idx=0, n = s.size(), k = sources.size();
         map<int, vector<pair<string, string>>> data;
-        string res = "";
 
         for(int i=0; i<k; i++)
             data[indices[i]].push_back({sources[i], targets[i]});
 
-        i = 0;
         for(const auto& [k, vec]: data) {
             for(const auto& [src, tar]: vec) {
                 indices[i] = k;
@@ -18,24 +16,44 @@ public:
             }
         }
 
-        i = 0;
-        while(i < n) {
-            while(i < n && (idx >= k || i < indices[idx])) {
-                res += s[i];
-                i++;
-            }
+        int unprocessed = indices[k - 1];
 
-            if(i >= n)
-                break;
+        // for(int i=0; i<k; i++)
+        //     cout<<indices[i]<<": "<<sources[i]<<" --> "<<targets[i]<<endl;
 
-            if(idx < k && s.substr(indices[idx], sources[idx].size()) == sources[idx]) {
-                res += targets[idx];
-                i += sources[idx].size(); 
-            }
+        for(int i=k-1; i>=0; i--) {
+            int idx = indices[i];
+            
+            if(idx > unprocessed)
+                continue;
 
-            idx++;
+            if(n - idx + 1 < sources[i].size())
+                continue;
+
+            if(s.substr(idx, sources[i].size()) != sources[i])
+                continue;
+
+            // Replace source with target
+            s.replace(idx, sources[i].size(), targets[i]);
+            unprocessed = idx - 1;
         }
 
-        return res;
+        return s;
     }
 };
+
+/*
+vmokgggqzp
+0123456789
+
+1: mo --> bfr
+3: kg --> s
+5: ggq --> so
+
+
+abcdef
+012345
+
+2: feg --> abc
+2: cdef --> feg
+*/
