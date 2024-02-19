3644fa24a21323e00888ddd4d8817b45c93ef3a4
Aditya Pranav Bhuvanapalli <adityapranavbhuvanapalli@gmail.com>
2024-02-19 23:22:44 +0530

    Time: 83 ms (53.07%) | Memory: 41.4 MB (58.50%) - LeetSync

--- 729-my-calendar-i/my-calendar-i.cpp
+++ 729-my-calendar-i/my-calendar-i.cpp
@@ -1,44 +1,35 @@
 class MyCalendar {
 public:
-    vector<vector<int>> calendar;
+    vector<pair<int, int>> calendar;
     MyCalendar() {
-        // calendar = {{10, 20}, {20, 30}, {40, 50}};
-        // cout<<binarySearch(5,15)<<endl;
-        // cout<<binarySearch(5,10)<<endl;
-        // cout<<binarySearch(25,40)<<endl;
-        // cout<<binarySearch(25,45)<<endl;
-        // cout<<binarySearch(30,45)<<endl;
+        
     }
     
     bool book(int start, int end) {
-        if(calendar.size() == 0) {
+        if(calendar.size() == 0 || calendar.back().second <= start) {
             calendar.push_back({start, end});
             return true;
         }
 
-        int n = calendar.size(), idx = binarySearch(start, end);
+        int idx = binarySearch(start);
 
-        // TODO: Should handle the case for last (idx will be n-1 but should be at n)
-        if(idx == n-1) {
-            if(calendar[idx][1] <= start) {
-                calendar.push_back({start, end});
-                return true;
-            }
+        auto& [x, y] = calendar[idx];
+
+        if(start <= x && end <= x) {
+            calendar.insert(calendar.begin() + idx, {start, end});
+            return true;
         }
 
-        if(end > calendar[idx][0])
-            return false;
-
-        calendar.insert(calendar.begin() + idx, {start, end});
-        return true;
+        return false;
     }
 
-    int binarySearch(int start, int end) {
-        int n = calendar.size(), l = 0, r = n - 1, mid;
+    int binarySearch(int val) {
+        int l = 0, n = calendar.size(), r = n - 1, mid;
+
         while(l < r) {
             mid = l + (r - l) / 2;
-        
-            if(calendar[mid][1] <= start) {
+
+            if(calendar[mid].second <= val) {
                 l = mid + 1;
             } else {
                 r = mid;
@@ -48,23 +39,10 @@ public:
         return l;
     }
 };
+
 /*
-10 - 20
-40 - 50
-15 - 25
-20 - 30
-
-10,20   20,30   40,50
-
-5,15
-5,10
-25,40
-25,45
-30,45
-
-Find first interval that is greater than start
+3,8     19,25   25,32   33,41   47,50   
 */
-
 /**
  * Your MyCalendar object will be instantiated and called as such:
  * MyCalendar* obj = new MyCalendar();
