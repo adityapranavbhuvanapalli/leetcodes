f2718b671b7b181097b25dec472bfc7e3c632f49
Aditya Pranav Bhuvanapalli <adityapranavbhuvanapalli@gmail.com>
2024-02-23 14:10:30 +0530

    Time: 155 ms (91.08%) | Memory: 100.5 MB (8.02%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.cpp
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.cpp
@@ -1,35 +1,48 @@
 class RandomizedSet {
 public:
-    int top = 0;
     unordered_map<int, int> index;
-    vector<int> v;
-    RandomizedSet() {}
-
+    vector<int> randomSet;
+    int top = 0;
+    RandomizedSet() {
+        
+    }
+    
     bool insert(int val) {
-        if (index.count(val))
+        if(index.count(val) == 1 && index[val] != -1)
             return false;
 
-        v.push_back(val);
-        index[val] = top++;
+        if(top < randomSet.size())
+            randomSet[top] = val;
+        else
+            randomSet.push_back(val);
+
+        index[val] = top;
+
+        top++;
         return true;
     }
-
+    
     bool remove(int val) {
-        if (index.count(val) == 0)
+        if(index.count(val) == 0 || index[val] == -1)
             return false;
 
-        int pos = index[val];
-        index[v[top - 1]] = pos;
-        index.erase(val);
-        swap(v.back(), v[pos]);
-        v.pop_back();
-        top--;
+        int valIdx = index[val];
+        int lastIdx = top - 1, lastVal = randomSet[top - 1];
 
+        swap(randomSet[valIdx], randomSet[lastIdx]);
+        index[lastVal] = valIdx;
+        index[val] = -1;
+        
+        top--;
         return true;
     }
-
-    int getRandom() { return v[rand() % v.size()]; }
+    
+    int getRandom() {
+        int random = rand() % top;
+        return randomSet[random];
+    }
 };
+
 /**
  * Your RandomizedSet object will be instantiated and called as such:
  * RandomizedSet* obj = new RandomizedSet();
