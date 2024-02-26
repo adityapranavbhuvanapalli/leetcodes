e9062b3c4c886d29e819969eb5b508b2230f8388
Aditya Pranav Bhuvanapalli <adityapranavbhuvanapalli@gmail.com>
2024-02-26 23:05:18 +0530

    Time: 20 ms (26.20%) | Memory: 20.9 MB (25.99%) - LeetSync

---
+++ 875-longest-mountain-in-array/longest-mountain-in-array.cpp
@@ -0,0 +1,71 @@
+class Solution {
+public:
+    int longestMountain(vector<int>& nums) {
+        int n = nums.size(), longest = 0, lenSoFar = 0;
+        int sign = 1;
+
+        if(n <= 1)
+            return 0;
+
+        int i = 0;
+
+        // Ignore the starting descent
+        while(i < n - 1 && nums[i] >= nums[i + 1])
+            i++;
+
+        while(i < n - 1) {
+            // Ascent
+            while(i < n - 1 && nums[i] < nums[i + 1]) {
+                lenSoFar++;
+                i++;
+            }
+
+            if(i == n - 1)
+                return longest;
+
+            if(lenSoFar == 0) {
+                i++;
+                continue;
+            }
+
+            if(nums[i] == nums[i + 1]) {
+                while(i < n - 1 && nums[i] == nums[i + 1])
+                    i++;
+
+                lenSoFar = 0;
+                continue;
+            }
+
+            if(i == n - 1)
+                return longest;
+
+            // For the peak
+            lenSoFar++;
+
+            // Descent
+            while(i < n - 1 && nums[i] > nums[i + 1]) {
+                lenSoFar++;
+                i++;
+            }
+
+            cout<<i<<" ";
+            longest = max(longest, lenSoFar);
+            lenSoFar = 0;
+        }
+
+        return longest;
+    }
+};
+
+/*          
+            7
+                        5
+        4
+                3
+2                   2
+    1
+
+                    i   j
+
+lenSoFar = 3
+*/
