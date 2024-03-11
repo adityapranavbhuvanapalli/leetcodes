a50eb4fbc800b0aadc4cc5950b041ee8c387b1a1
Aditya Pranav Bhuvanapalli <adityapranavbhuvanapalli@gmail.com>
2024-03-11 20:00:51 +0530

    Time: 47 ms (49.04%) | Memory: 27.6 MB (20.67%) - LeetSync

---
+++ 2348-count-number-of-texts/count-number-of-texts.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    const int MOD = 1e9 + 7;
+    vector<string> ref = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    vector<int> dp;
+    int countTexts(string pressedKeys) {
+        int n = pressedKeys.size();
+        dp = vector<int>(n, -1);
+        return solve(0, n, pressedKeys);
+    }
+
+    int solve(int i, int n, string& pressedKeys) {
+        if(i >= n)
+            return 1;
+
+        if(dp[i] != -1)
+            return dp[i];
+
+        int sum = 0;
+        int sz = ref[pressedKeys[i] - '0'].size();
+
+        for(int j=0; j<sz; j++) {
+            if(i + j >= n || pressedKeys[i + j] != pressedKeys[i])
+                break;
+
+            sum = (sum + solve(i + j + 1, n, pressedKeys)) % MOD;
+        }
+
+        return dp[i] = sum;
+    }
+};
+
+/*
+2
+    2
+        2
+            3
+                3(aaadd)
+            33(aaae)
+    22
+        3   
+            3(abdd)
+        33(abe)
+22  
+    2
+        3
+            3(badd)
+        33(bae)
+222
+    3
+        3(cdd)
+    33(ce)
+*/
