f0d8b9ef752fb95d390352936f8825ae211b13e3
Aditya Pranav Bhuvanapalli <adityapranavbhuvanapalli@gmail.com>
2024-03-12 10:52:35 +0530

    Time: 10 ms (35.49%) | Memory: 16.4 MB (5.07%) - LeetSync

---
+++ 1267-remove-zero-sum-consecutive-nodes-from-linked-list/remove-zero-sum-consecutive-nodes-from-linked-list.cpp
@@ -0,0 +1,58 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeZeroSumSublists(ListNode* head) {
+        int sum = 0;
+        unordered_map<int, ListNode*> prefix;
+        unordered_map<ListNode*, int> sums;
+        ListNode *cur = head, *start = head;
+        
+        while(cur) {
+            sum += cur->val;
+            if(sum == 0) {
+                while(start != cur) {
+                    prefix.erase(sums[start]);
+                    start = start->next;
+                }
+                start = cur->next;
+                cur = start;
+                continue;
+            }
+
+            if(prefix.count(sum) == 1) {
+                // Then remove all elements from prefix[sum] till cur
+                // While deleting clear the elements addresses in prefix
+                ListNode* tmp = prefix[sum]->next;
+                while(tmp != cur) {
+                    prefix.erase(sums[tmp]);
+                    tmp = tmp->next;
+                }
+                prefix[sum]->next = cur->next;
+            } else {
+                // Or else move to the next one
+                prefix[sum] = cur;
+                sums[cur] = sum;
+            }
+            cur = prefix[sum]->next;
+        }
+
+        return sum == 0 ? nullptr : start;
+    }
+};
+
+/*
+1   2   3   -3  -2  -1
+1   0
+
+2   -1  0
+2   1
+*/
