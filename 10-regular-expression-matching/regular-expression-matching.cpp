6464abfe59e279bd83280b7835c8ed5c8b72fe31
Aditya Pranav Bhuvanapalli <adityapranavbhuvanapalli@gmail.com>
2024-03-12 12:23:05 +0530

    Time: 3 ms (68.48%) | Memory: 15.8 MB (10.19%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.cpp
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -1,24 +1,39 @@
 class Solution {
 public:
-    map<pair<int, int>, int> dp; 
+    map<pair<int, int>, int> dp;
     bool isMatch(string s, string p) {
-        int sLen = s.size(), pLen = p.size();
-        return solve(0, 0, sLen, pLen, s, p);
+        int ns = s.size(), np = p.size();
+        return solve(0, 0, ns, np, s, p);    
     }
 
-    bool solve(int i, int j, int sLen, int pLen, string& s, string& p) {
+    bool solve(int i, int j, int ns, int np, string& s, string& p) {
+        if(j == np)
+            return i == ns;
+
         if(dp.count({i, j}) == 1)
             return dp[{i, j}];
 
-        if(j == pLen)
-            return dp[{i, j}] = (i == sLen);
+        bool match = i < ns && (p[j] == '.' || s[i] == p[j]);
 
-        bool match = i<sLen && (s[i] == p[j] || p[j] == '.');
-
-        if(j+1 < pLen && p[j+1] == '*') 
-            return dp[{i, j}] = solve(i, j+2, sLen, pLen, s, p) 
-                || (match && solve(i+1, j, sLen, pLen, s, p));
-
-        return dp[{i, j}] = (match && solve(i+1, j+1, sLen, pLen, s, p));       
+        if(j + 1 < np && p[j + 1] == '*') 
+            return dp[{i, j}] = (match && solve(i+1, j, ns, np, s, p)) || solve(i, j+2, ns, np, s, p); 
+             
+        return dp[{i, j}] = match && solve(i+1, j+1, ns, np, s, p);
     }
-};
+}; 
+
+/*
+aa
+a
+
+a*
+a
+
+check match
+check for next char 
+if * is there
+    1. solve for i, j+2
+    2. solve for i+1, j if match
+
+solve(i+1, j+1) if match
+*/
