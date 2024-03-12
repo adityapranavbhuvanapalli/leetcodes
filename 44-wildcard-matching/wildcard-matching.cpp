4e4d9a29c73df2ef908276ecece2a1aa763d0f05
Aditya Pranav Bhuvanapalli <adityapranavbhuvanapalli@gmail.com>
2024-03-12 13:22:44 +0530

    Time: 475 ms (5.21%) | Memory: 103.9 MB (5.00%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,24 +1,28 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
+    map<pair<int, int>, int> dp;
     bool isMatch(string s, string p) {
-        int ns = s.size(), np = p.size();
-        dp = vector<vector<int>>(ns+1, vector<int>(np+1, -1));
-        return solve(0, 0, ns, np, s, p);
+        int sLen = s.size(), pLen = p.size();
+        return solve(0, 0, sLen, pLen, s, p);
     }
 
-    bool solve(int i, int j, int ns, int np, string& s, string& p) {
-        if(j == np)
-            return (i == ns);
+    bool solve(int i, int j, int sLen, int pLen, string& s, string& p) {
+        if(j == pLen)
+            return i == sLen;
 
-        if(dp[i][j] != -1)
-            return dp[i][j];
+        if(dp.count({i, j}) == 1)
+            return dp[{i, j}];
 
         if(p[j] == '*') {
-            // Either consider 1 or consider none
-            return dp[i][j] = (i < ns && solve(i+1, j, ns, np, s, p)) || solve(i, j+1, ns, np, s, p);
-        }
+            return dp[{i, j}] = solve(i, j+1, sLen, pLen, s, p) || (i < sLen && solve(i+1, j, sLen, pLen, s, p));
+        } 
 
-        return dp[i][j] = i < ns && (s[i] == p[j] || p[j] == '?') && solve(i+1, j+1, ns, np, s, p); 
+        bool match = (i < sLen && s[i] == p[j]) || p[j] == '?';
+        return dp[{i, j}] = match && solve(i+1, j+1, sLen, pLen, s, p);
     }
 };
+
+/*
+aa
+a
+*/
