240c9461814cda9be9673a0bccd11ca035732725
Aditya Pranav Bhuvanapalli <adityapranavbhuvanapalli@gmail.com>
2024-03-14 11:51:24 +0530

    Time: 41 ms (47.85%) | Memory: 38.3 MB (20.69%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,32 +1,21 @@
 class Solution {
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-        int n = nums.size(), l = 0, r = 0, res = 0, sum = 0, count = 0;
-        
-        while(r < n) {
-            if(nums[r] == 1) {
-                sum++;
-                count = 0;
+        unordered_map<int, int> prefixFreq;
+        int sum = 0, res = 0;
+
+        for(const auto& num: nums) {
+            sum += num;
+            if(sum == goal)
+                res++;
+            
+            if(prefixFreq.count(sum - goal) == 1) {
+                res += prefixFreq[sum - goal];
             }
 
-            while(l <= r && sum >= goal) {
-                if(sum == goal)
-                    count++;
-                sum -= nums[l++];
-            }
-
-            res += count;
-            r++;
+            prefixFreq[sum]++;
         }
 
         return res;
     }
 };
-
-/*
-sum = 2
-
-1   0   1   0   1
-1   0
-
-*/
