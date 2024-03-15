a5f5607fded2852fef1c4b4bf11276c17ce26a99
Aditya Pranav Bhuvanapalli <adityapranavbhuvanapalli@gmail.com>
2024-03-15 10:03:29 +0530

    Time: 13 ms (76.05%) | Memory: 26.4 MB (72.09%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -2,28 +2,47 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
-        vector<int> prefix(n, 1);
-        vector<int> suffix(n, 1);
-        vector<int> res(n);
+        vector<int> res(n, 1);
 
         int tmp = 1;
-        for(int i=0; i<n; i++) {
-            prefix[i] = tmp * nums[i];
-            tmp = prefix[i];
+        for(int i=n-1; i>=0; i--) {
+            res[i] = tmp * nums[i];
+            tmp = res[i];
         }
 
         tmp = 1;
-        for(int i=n-1; i>=0; i--) {
-            suffix[i] = tmp * nums[i];
-            tmp = suffix[i];
+        for(int i=0; i<n; i++) {
+            nums[i] = tmp * nums[i];
+            tmp = nums[i];
         }
 
+        // cout<<"Prefix: ";
+        // for(const auto& a: nums)
+        //     cout<<a<<" ";
+        // cout<<endl;
+
+        
+        // cout<<"Suffix: ";
+        // for(const auto& a: res)
+        //     cout<<a<<" ";
+        // cout<<endl;   
+
         for(int i=0; i<n; i++) {
-            int left = (i > 0) ? prefix[i - 1] : 1;
-            int right = (i < n - 1) ? suffix[i + 1] : 1;
+            int left = (i > 0) ? nums[i - 1] : 1;
+            int right = (i < n - 1) ? res[i + 1] : 1;
             res[i] = left * right;
         }
 
         return res;
     }
 };
+
+/*
+1   2   3   4
+
+res =   24  24  12  4   (suffix)
+nums =  1   2   6   24  (prefix)
+            i
+
+res =   24  12  8   6
+*/
