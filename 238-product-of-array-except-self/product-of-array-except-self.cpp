753443ad5e0721892f4c7cd4e58e5ee92af20fdf
Aditya Pranav Bhuvanapalli <adityapranavbhuvanapalli@gmail.com>
2024-03-15 10:08:32 +0530

    Time: 15 ms (67.47%) | Memory: 26.3 MB (86.12%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -4,18 +4,21 @@ public:
         int n = nums.size();
         vector<int> res(n, 1);
 
+        // Storing suffix in res
         int tmp = 1;
         for(int i=n-1; i>=0; i--) {
             res[i] = tmp * nums[i];
             tmp = res[i];
         }
 
+        // Storing prefix in nums
         tmp = 1;
         for(int i=0; i<n; i++) {
             nums[i] = tmp * nums[i];
             tmp = nums[i];
         }
 
+        // Getting the final result
         for(int i=0; i<n; i++) {
             int left = (i > 0) ? nums[i - 1] : 1;
             int right = (i < n - 1) ? res[i + 1] : 1;
