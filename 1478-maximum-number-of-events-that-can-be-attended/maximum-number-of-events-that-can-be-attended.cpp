6b13dcaa25e242e2f4c77d2d2fb5dcfcb5a71e7b
Aditya Pranav Bhuvanapalli <adityapranavbhuvanapalli@gmail.com>
2024-03-16 22:36:58 +0530

    Time: 206 ms (76.55%) | Memory: 74.3 MB (50.73%) - LeetSync

--- 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.cpp
+++ 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.cpp
@@ -3,7 +3,7 @@ public:
     int maxEvents(vector<vector<int>>& events) {
         int i = 0, n = events.size(), res = 0;
         sort(events.begin(), events.end());
-        priority_queue <int, vector<int>, greater<int>> pq;
+        priority_queue<int, vector<int>, greater<int>> pq;
         
         for(int d=1; d<=1e5; d++) {
             while(pq.size() && pq.top() < d)
@@ -25,11 +25,18 @@ public:
 };
 
 /*
-1   2
-    2   3
-        3   4
+0-  1
+1-  1   2   3
+2-          3   4
+3-              4
+4-              4
+------------------
+d=0
 
-1,2
-1,2
+1,1
+1,3
+3,4
+4,4
+4,4
 
 */
