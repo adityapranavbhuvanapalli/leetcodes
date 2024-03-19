478a6ec837a746e3b4291f652ac514ccd3a7c1cd
Aditya Pranav Bhuvanapalli <adityapranavbhuvanapalli@gmail.com>
2024-03-19 10:36:16 +0530

    Time: 99 ms (33.15%) | Memory: 53.4 MB (25.16%) - LeetSync

---
+++ 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    const int MOD = 1e9 + 7;
+    int numSubseq(vector<int>& nums, int target) {
+        int res = 0, n = nums.size();
+
+        sort(nums.begin(), nums.end());
+
+        vector<int> power(n + 1, 1);
+
+        for(int i=1; i<=n; i++) {
+            power[i] = (power[i - 1] * 2) % MOD;
+        }
+
+        for(int i=0; i<n; i++) {
+            int val = target - nums[i];
+
+            if(target < 2 * nums[i])
+                break;
+
+            int idx = binarySearch(i, n - 1, nums, val);
+            int len = idx - i + 1;
+            
+            res = (res + power[len - 1]) % MOD;
+        }
+
+        return res;
+    }
+
+    int binarySearch(int left, int right, vector<int>& nums, int val) {
+        int l = left, r = right, mid;
+
+        while(l < r) {
+            mid = l + ceil((r - l) / 2.0);
+
+            if(nums[mid] <= val) {
+                l = mid;
+            } else {
+                r = mid - 1;
+            }   
+        }
+
+        return r;
+    }
+};
+
+/*
+11
+3   3   6   8   9
+l
+            r
+
+2^(len - 1)
+
+3, 33, 36, 38, 336, 338, 368, 3368 - 8
+3, 36, 38, 368 - 4
+*/
