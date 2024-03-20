16f5e99c4c762ae3240e1c10194c26f6976965ea
Aditya Pranav Bhuvanapalli <adityapranavbhuvanapalli@gmail.com>
2024-03-20 22:52:35 +0530

    Time: 272 ms (74.79%) | Memory: 163 MB (38.25%) - LeetSync

--- 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.cpp
+++ 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.cpp
@@ -3,38 +3,44 @@ public:
     vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) {
         int n = recipes.size();
         vector<string> res;
-        unordered_set<string> recipeSet(recipes.begin(), recipes.end());
         unordered_map<string, vector<string>> graph;
         unordered_map<string, int> incoming;
         queue<string> q;
 
+        // Graph
         for(int i=0; i<n; i++) {
-            for(int j=0; j<ingredients[i].size(); j++) {
+            for(const auto& ingredient: ingredients[i]) {
+                graph[ingredient].push_back(recipes[i]);
                 incoming[recipes[i]]++;
-                graph[ingredients[i][j]].push_back(recipes[i]);
             }
         }
 
-        // Topological Sort
-        for(auto& supply: supplies)
+        for(const auto& supply: supplies)
             q.push(supply);
 
+        // Topological Sort
         while(q.size()) {
-            auto& cur = q.front();
+            string front = q.front();
+            q.pop();
 
-            if(recipeSet.count(cur))
-                res.push_back(cur);
+            if(incoming.count(front) == 1)
+                res.push_back(front);
 
-            for(auto& adj: graph[cur]) {
+            for(const auto& adj: graph[front]) {
                 incoming[adj]--;
                 if(incoming[adj] == 0) {
                     q.push(adj);
                 }
             }
-
-            q.pop();
         }
 
         return res;
     }
 };
+
+/*
+yeast - bread
+flour - bread
+bread - samdwich
+meat - sandwich
+*/
