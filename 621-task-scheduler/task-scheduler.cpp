148e4aadf6e6041ab1f71a9fab044d72aa00d824
Aditya Pranav Bhuvanapalli <adityapranavbhuvanapalli@gmail.com>
2024-03-20 10:05:42 +0530

    Time: 75 ms (48.69%) | Memory: 38.2 MB (58.35%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -6,33 +6,27 @@ public:
         priority_queue<int> pq;
         stack<int> rem;
 
-        for(const auto& task: tasks)
+        for (const auto& task : tasks)
             freq[task]++;
-    
-        for(const auto& [k, v]: freq)
+
+        for (const auto& [k, v] : freq)
             pq.push(v);
 
-        while(pq.size()) {
-            // Select n tasks or wait if no tasks are there
-            for(int i=0; i<=n; i++) {
-                if(pq.empty()) {
-                    if(rem.size())
+        while (pq.size()) {
+            for (int i = 0; i <= n; i++) {
+                if (pq.empty()) {
+                    if (rem.size() > 0)
                         time += n - i + 1;
-                    // cout<<n<<":"<<i<<" ";
                     break;
                 }
 
-                if(pq.top() > 1)
+                time++;
+                if (pq.top() > 1)
                     rem.push(pq.top() - 1);
                 pq.pop();
-
-                time++;
-                // cout<<rem.top()<<":"<<time<<":"<<rem.size()<<" --- ";
-
             }
 
-            // cout<<" ----> "<<time<<endl;
-            while(rem.size()) {
+            while (rem.size()) {
                 pq.push(rem.top());
                 rem.pop();
             }
@@ -41,3 +35,19 @@ public:
         return time;
     }
 };
+
+/*
+A - 3
+B - 3
+
+pq:
+3
+
+rem:
+2
+
+_   _   _
+A   B   #
+A   B   #
+A   B   #
+*/
