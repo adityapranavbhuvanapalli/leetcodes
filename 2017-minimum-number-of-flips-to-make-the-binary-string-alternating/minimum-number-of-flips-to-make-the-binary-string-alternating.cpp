c00347277e88ce29c23c1bf8060a608c8820a342
Aditya Pranav Bhuvanapalli <adityapranavbhuvanapalli@gmail.com>
2024-03-22 13:52:28 +0530

    Time: 32 ms (82.85%) | Memory: 12.6 MB (81.40%) - LeetSync

--- 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
+++ 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
@@ -4,22 +4,17 @@ public:
         int l = 0, n = s.size(), minVal = n, count1 = 0, count2 = 0;
         char a = '1', b = '0';
 
-        s += s;
         for(int r=0; r<2*n; r++) {
-            count1 += (s[r] != a);
-            count2 += (s[r] != b);
-
-            // cout<<l<<" "<<r<<": "<<count1<<" "<<count2<<endl;
-            // cout<<(s[r]!=a);
-            // cout<<count1;
+            count1 += (s[r % n] != a);
+            count2 += (s[r % n] != b);
             
             if(r - l + 1 > n) {
                 if(n % 2 == 0) {
-                    count1 -= (s[l] != a);
-                    count2 -= (s[l] != b);
+                    count1 -= (s[l % n] != a);
+                    count2 -= (s[l % n] != b);
                 } else {
-                    count1 -= (s[l] != b);
-                    count2 -= (s[l] != a);
+                    count1 -= (s[l % n] != b);
+                    count2 -= (s[l % n] != a);
                 }
                 l++;
             }
