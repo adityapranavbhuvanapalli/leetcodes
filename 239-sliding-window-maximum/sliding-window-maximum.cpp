af3844b6ed4a91cc16505c2cbbb1891d8a1455e9
Aditya Pranav Bhuvanapalli <adityapranavbhuvanapalli@gmail.com>
2024-03-23 15:34:17 +0530

    Time: 183 ms (42.50%) | Memory: 137.2 MB (46.99%) - LeetSync

---
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n = nums.size(), l = 0;
+        vector<int> res;
+        deque<int> dq;
+
+        for(int r=0; r<n; r++) {
+            // Check if front of dq is valid or not
+            // Insert the current element in the back at correct position
+            // Add front to res
+            if(r - l + 1 > k) {
+                l++;
+                while(dq.size() && l > dq.front()) 
+                    dq.pop_front();
+            }  
+
+            while(dq.size() && nums[dq.back()] <= nums[r])
+                dq.pop_back();
+
+            dq.push_back(r);
+
+            if(r - l + 1 == k)
+                res.push_back(nums[dq.front()]);
+        }
+
+        return res;
+    }
+};
+
+/*
+0   1   2   3   4   5   6   7
+1   3   -1  -3  5   3   6   7
+                    l
+                            r
+
+dq:
+7
+7   
+
+res:
+3   3   5   5   6
+*/
