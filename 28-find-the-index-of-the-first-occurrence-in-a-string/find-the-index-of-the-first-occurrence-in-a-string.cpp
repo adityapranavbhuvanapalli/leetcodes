19865c9ed694357e7b96efd31887f92bd174a27c
Aditya Pranav Bhuvanapalli <adityapranavbhuvanapalli@gmail.com>
2024-03-25 11:05:30 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (80.03%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -5,8 +5,9 @@ public:
         int i, j, prevlps;
         vector<int> lps(m, 0);
 
-        // Pre Processing
-        prevlps = 0, i = 1;
+        // Pre-processing
+        // Find for each i, from 0 to i, the longest common prefix which is also a suffix
+        i = 1, prevlps = 0;
         while(i < m) {
             if(needle[i] == needle[prevlps]) {
                 lps[i] = prevlps + 1;
@@ -21,7 +22,7 @@ public:
             }
         }
 
-        // Matching
+        // Compare
         i = 0, j = 0;
         while(i < n) {
             if(haystack[i] == needle[j]) {
@@ -34,10 +35,24 @@ public:
                 }
             }
 
-            if(j == m)
+            if(j == m) {
+                j = lps[j - 1];
                 return i - m;
+            }
         }
-
+    
         return -1;
     }
-};
+};  
+
+/*
+ p
+abcdab
+     i
+000012
+
+p
+ab
+ i
+0
+*/
