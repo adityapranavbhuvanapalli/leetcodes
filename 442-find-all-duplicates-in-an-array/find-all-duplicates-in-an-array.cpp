a7548987cb7bdc09c4fe921d97375d758fcbcecb
Aditya Pranav Bhuvanapalli <adityapranavbhuvanapalli@gmail.com>
2024-03-25 10:01:46 +0530

    Time: 25 ms (98.64%) | Memory: 36 MB (68.64%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
@@ -5,7 +5,7 @@ public:
         vector<int> res;
 
         while(i < n) {
-            while(i < n && nums[i] - 1 < n && nums[nums[i] - 1] != nums[i] && nums[i] != i + 1) {
+            while(nums[nums[i] - 1] != nums[i] && nums[i] != i + 1) {
                 swap(nums[i], nums[nums[i] - 1]);
             }
 
