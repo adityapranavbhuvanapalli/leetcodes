bef190771d50cd311f3fdef105d88a047360a168
Aditya Pranav Bhuvanapalli <adityapranavbhuvanapalli@gmail.com>
2024-03-25 12:53:55 +0530

    Time: 156 ms (91.24%) | Memory: 137 MB (88.78%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -6,19 +6,16 @@ public:
         deque<int> dq;
 
         for(int r=0; r<n; r++) {
-            // Check if front of dq is valid or not
-            // Insert the current element in the back at correct position
-            // Add front to res
-            if(r - l + 1 > k) {
-                l++;
-                while(dq.size() && l > dq.front()) 
-                    dq.pop_front();
-            }  
-
             while(dq.size() && nums[dq.back()] < nums[r])
                 dq.pop_back();
 
             dq.push_back(r);
+
+            if(r - l + 1 > k) {
+                l++;
+                if(dq.front() < l)
+                    dq.pop_front();
+            }
 
             if(r - l + 1 == k)
                 res.push_back(nums[dq.front()]);
@@ -29,15 +26,10 @@ public:
 };
 
 /*
-0   1   2   3   4   5   6   7
-1   3   -1  -3  5   3   6   7
-                    l
-                            r
+1   3   -1  -3  2   3   6   7
+        l
+                r   
 
-dq:
-7
-7   
-
-res:
-3   3   5   5   6
+dq: 
+2
 */
