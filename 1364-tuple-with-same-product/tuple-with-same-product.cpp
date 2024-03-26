7459a0db49be14ca2ffce005ecbabff43937960e
Aditya Pranav Bhuvanapalli <adityapranavbhuvanapalli@gmail.com>
2024-03-26 22:32:45 +0530

    Time: 347 ms (55.60%) | Memory: 84.8 MB (63.20%) - LeetSync

---
+++ 1364-tuple-with-same-product/tuple-with-same-product.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int tupleSameProduct(vector<int>& nums) {
+        int n = nums.size(), res = 0;
+        unordered_map<int, int> freq;
+
+        for(int i=0; i<n; i++) {
+            for(int j=i+1; j<n; j++) {
+                freq[nums[i] * nums[j]]++;
+            }
+        }
+
+        for(const auto& [k, v]: freq) {
+            res += v * (v - 1) / 2;
+        }
+
+        return 8 * res;
+    }
+};
+
+/*
+1   2   4   5   10
+
+2 - 1
+4 - 1
+5 - 1
+10 - 2 - 2C2 = 1
+8 - 1
+20 - 2 - 2C2 = 1
+40 - 1
+50 - 1
+
+nC2 = n * (n - 1) / 2
+*/
