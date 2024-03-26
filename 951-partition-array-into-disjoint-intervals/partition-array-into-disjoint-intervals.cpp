5403df1abf39bb127941d9a3630bb81fce55ec05
Aditya Pranav Bhuvanapalli <adityapranavbhuvanapalli@gmail.com>
2024-03-26 22:52:20 +0530

    Time: 111 ms (45.14%) | Memory: 96.7 MB (65.84%) - LeetSync

--- 951-partition-array-into-disjoint-intervals/partition-array-into-disjoint-intervals.cpp
+++ 951-partition-array-into-disjoint-intervals/partition-array-into-disjoint-intervals.cpp
@@ -1,27 +1,36 @@
 class Solution {
 public:
     int partitionDisjoint(vector<int>& nums) {
-        int maxSoFar = -1, n = nums.size();
         stack<int> s;
+        int maxVal = 0, n = nums.size();
 
-        for(int i=n-1; i>=0; i--) {
-            if(!s.size() || nums[s.top()] > nums[i])
+        for(int i=n-1; i>0; i--) {
+            if(s.empty() || nums[s.top()] > nums[i])
                 s.push(i);
         }
 
-        for(int i=0; i<n; i++) {
-            maxSoFar = max(maxSoFar, nums[i]);
-            if(s.size() && s.top() == i)
-                s.pop();
+        for(int i=0; i<n-1; i++) {
+            maxVal = max(maxVal, nums[i]);
 
-            if(maxSoFar <= nums[s.top()]) 
-                return i+1;
+            if(s.top() == i)
+                s.pop();
+            
+            if(nums[s.top()] >= maxVal)
+                return i + 1;
         }
 
-        return n;
+        return -1;
     }
 };
 
 /*
-1   1   1   6   0   12     
+5   0   3   8   6
+max       min
+
+5   0   3   8   6
+        0   3   6
+
+6   8   3   0   5
+
+6   3   0
 */
