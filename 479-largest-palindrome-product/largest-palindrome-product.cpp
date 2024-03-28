43445bbd3b955c057062ef0ab013f48a6b919d1d
Aditya Pranav Bhuvanapalli <adityapranavbhuvanapalli@gmail.com>
2024-03-28 10:32:15 +0530

    Time: 0 ms (100.00%) | Memory: 7.1 MB (96.20%) - LeetSync

---
+++ 479-largest-palindrome-product/largest-palindrome-product.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int largestPalindrome(int n) {
+        vector<int> res{0, 9, 987, 123, 597, 677, 1218, 877, 475};
+        return res[n];
+    }
+};
+
+/*
+99999999 * 99999999
+
+1111111111111111
+*/
